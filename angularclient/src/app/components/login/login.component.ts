34c54480ed0c67a9f785f2c4832e5c083773a25f
Gael67350 <gael67350@gmail.com>
2021-03-12 09:15:28 +0100

    Implémentation du formulaire de connexion

--- angularclient/src/app/components/login/login.component.ts
+++ angularclient/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 import { AuthenticationService } from '../../services/authentication.service';
 
@@ -9,8 +11,15 @@ import { AuthenticationService } from '../../services/authentication.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = "";
 
   constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService
     ) {
@@ -20,6 +29,33 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      mail: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.login(this.f.mail.value, this.f.password.value)
+    .pipe(first())
+    .subscribe(data => {
+      this.router.navigate([this.returnUrl]);
+    },
+    error => {
+      this.error = error;
+      this.loading = false;
+    });
   }
 
 }
